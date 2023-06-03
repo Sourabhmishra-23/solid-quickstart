f079f820f7cb54cc5b5252bbbf4bef1b178e2af7
Sourabh Mishra <59117474+Sourabhmishra-23@users.noreply.github.com>
2023-06-03 19:33:47 +0530

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,15 +4,15 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>Asha Jyotish Parivar</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Page Under processing</h1>
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+
+
         </a>{" "}
-        to learn how to build SolidStart apps.
+        Asha Jyotish Parivar Seva Samiti 
       </p>
     </main>
   );
